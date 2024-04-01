8df94e82866d7857bc06d370e683265ea3283b19
MINHCHI <93319143+Ca-mega3-151@users.noreply.github.com>
2024-04-01 17:09:57 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import { shuffled } from "./utils/array";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  components: { MainScreen, InteractScreen },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+    };
+  },
+  methods: {
+    onHandleBeforeStart(configs) {
+      this.settings.totalOfBlocks = configs.totalOfBlocks;
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+      this.settings.cardsContext = shuffled(shuffled(shuffled(cards)));
+      this.settings.startedAt = new Date().getTime();
+      //data ready
+
+      this.statusMatch = "match";
+    },
+  },
+};
+</script>
---
+++ src/components/InteractScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="screen">
+    <h1>Ineract Components here...</h1>
+    <card-flip v-for="(card, index) in cardContext" :key="index" />
+  </div>
+</template>
+
+<script>
+import CardFlip from "./AppCard.vue";
+export default {
+  props: {
+    cardsContext: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+  components: {
+    CardFlip,
+  },
+};
+</script>
